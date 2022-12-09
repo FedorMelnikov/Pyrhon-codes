849b65edaafa81438a067a3308524a8b7b9687e3
FedorMelnikov <120166550+FedorMelnikov@users.noreply.github.com>
2022-12-09 09:29:12 +0100

    Add files via upload

---
+++ replace.py
@@ -0,0 +1,10 @@
+# Ask user to enter some numbers
+integral_1 = int(input("write the int "))
+integral_2 = int(input("write the int "))
+integral_3 = int(input("write the int "))
+
+# Calculate results
+print(integral_1 + integral_2 + integral_3)
+print(integral_1 - integral_2)
+print(integral_1 * integral_3)
+print((integral_1 + integral_2 + integral_3) / integral_3)
