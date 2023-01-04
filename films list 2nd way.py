8e7806339791f1451d551ccc7e4e1aedefce2b24
FedorMelnikov <120166550+FedorMelnikov@users.noreply.github.com>
2023-01-04 20:09:19 +0400

    Update films list 2nd way.py

--- films list 2nd way.py
+++ films list 2nd way.py
@@ -1,5 +1,3 @@
 movies = ["Titanic", "Drive", "Robocop", "Borat", "Mimino"]
-count1 = 0
 for count, movie in enumerate(movies):
-    count1 += 1
-    print(f"Movie {count1}: {movies[count]}")
+    print(f"Movie {count + 1}: {movie}")
